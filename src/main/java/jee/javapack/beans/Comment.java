4753439108508aff019d65fe3e041862aff07f8e
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-05-02 17:06:37 +0100

    add les class and DAO

---
+++ src/main/java/jee/javapack/beans/Comment.java
@@ -0,0 +1,10 @@
+package jee.javapack.beans;
+
+public class Comment {
+    private Long id;
+    private Long filmId;
+    private Long userId;
+    private String commentText;
+
+
+}
---
+++ src/main/java/jee/javapack/dao/FilmRatingDAO.java
@@ -0,0 +1,14 @@
+package jee.javapack.dao;
+
+import jee.javapack.beans.FilmRating;
+
+import java.util.List;
+
+public interface FilmRatingDAO {
+    void addFilmRating(FilmRating filmRating);
+    void updateFilmRating(FilmRating filmRating);
+    FilmRating getFilmRatingById(Long id);
+    List<FilmRating> getFilmRatingsByFilmId(Long filmId);
+    List<FilmRating> getFilmRatingsByUserId(Long userId);
+    void deleteFilmRating(Long id);
+}
