63aa48cf5e06ca87516b9b47c0b17af8bf90ea0f
Noureddine Bouzire <bouzire.noureddine1@gmail.com>
2024-05-02 14:46:38 +0100

    creating the PlaylistServlet

--- src/main/java/jee/javapack/servlets/PlaylistServlet.java
+++ src/main/java/jee/javapack/servlets/PlaylistServlet.java
@@ -1,16 +1,17 @@
 package jee.javapack.servlets;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
 
-@WebServlet(name = "PlaylistServlet", value = "/PlaylistServlet")
+@WebServlet(name = "playlist", value = "/playlist")
 public class PlaylistServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
+        // Forward the request to playlist.jsp
+        request.getRequestDispatcher("/playlist.jsp").forward(request, response);
     }
-
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
