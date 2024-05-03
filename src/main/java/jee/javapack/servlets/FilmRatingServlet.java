8eb7e32b8d7aa9a914ed7d3b1fb3bcde6f49a364
chaimaaenaa <mekayssichaimaaenaa@gmail.com>
2024-05-03 17:23:41 +0100

    modifier commentServlet FilmRatingServlet

--- src/main/java/jee/javapack/servlets/FilmRatingServlet.java
+++ src/main/java/jee/javapack/servlets/FilmRatingServlet.java
@@ -18,18 +18,16 @@ public class FilmRatingServlet extends HttpServlet {
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Récupérer les données de la requête
+
         long filmId = Long.parseLong(request.getParameter("filmId"));
         long userId = Long.parseLong(request.getParameter("userId"));
         int rating = Integer.parseInt(request.getParameter("rating"));
 
-        // Créer un objet FilmRating avec les données récupérées
+
         FilmRating filmRating = new FilmRating(filmId, userId, rating);
 
-        // Appeler la méthode addFilmRating du DAO pour ajouter la note de film dans la base de données
         filmRatingDAO.addFilmRating(filmRating);
 
-        // Rediriger l'utilisateur vers une page de confirmation ou une autre page de votre choix
-        response.sendRedirect("confirmation.html");
+        response.sendRedirect("confirmation.jsp");
     }
 }
